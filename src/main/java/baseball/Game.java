2421477f40667f3e4fb841bde53c7095aea6e56a
Jongkook <barley.son@Jongkooks-MacBook-Pro.local>
2022-04-16 19:49:00 +0900

    feat: show retry message and game over message

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -14,10 +14,28 @@ public class Game {
     private final String TEXT_RESULT_BALL = "볼";
     private final String TEXT_RESULT_NOTHING = "낫싱";
     private final String TEXT_RESULT_CORRECT = "3개의 숫자를 모두 맞히셨습니다! 게임종료";
+    private final String TEXT_RETRY_OR_EXIT = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    private final String TEXT_GAME_OVER = "\n\n<<<<< GAME OVER >>>>>\n\n";
 
     List<Integer> answer = new ArrayList<>();
 
-    Game() {
+    void play() {
+        makeAnswer();
+        boolean isPlaying = true;
+        while (isPlaying) {
+            System.out.print(TEXT_INPUT_NUMBER);
+            String playerInput = Console.readLine();
+            List<Integer> input = getInputFromString(playerInput);
+            int sCount = countStrike(input);
+            int bCount = countBall(input);
+            showStrikeAndBall(sCount, bCount);
+            isNothing(sCount, bCount);
+            isPlaying = !isGameOver(sCount);
+        }
+        retryOrExit();
+    }
+
+    void makeAnswer() {
         // 중복 제거를 위한 HashSet
         HashSet<Integer> set = new HashSet<>(NUMBER_COUNT);
         while (set.size() < NUMBER_COUNT) {
@@ -31,18 +49,18 @@ public class Game {
         }
     }
 
-    void play() {
-        boolean isPlaying = true;
-        while (isPlaying) {
-            System.out.print(TEXT_INPUT_NUMBER);
-            String playerInput = Console.readLine();
-            List<Integer> input = getInputFromString(playerInput);
-            int sCount = countStrike(input);
-            int bCount = countBall(input);
-            showStrikeAndBall(sCount, bCount);
-            isNothing(sCount, bCount);
-            isPlaying = !isGameOver(sCount);
+    void retryOrExit() {
+        System.out.println(TEXT_RETRY_OR_EXIT);
+        String playerInput = Console.readLine();
+        if (playerInput.equals("1")) {
+            play();
+            return;
         }
+        if (playerInput.equals("2")) {
+            System.out.println(TEXT_GAME_OVER);
+            return;
+        }
+        throw new IllegalArgumentException();
     }
 
     List<Integer> getInputFromString(String playerInput) {
@@ -97,11 +115,13 @@ public class Game {
             System.out.println(ball + TEXT_RESULT_BALL);
             return;
         }
-        if (ball == 0) {
+        if (ball == 0 && strike < 3) {
             System.out.println(strike + TEXT_RESULT_STRIKE);
             return;
         }
-        System.out.println(strike + TEXT_RESULT_STRIKE + ", " + ball + TEXT_RESULT_BALL);
+        if (ball > 0 && strike > 0) {
+            System.out.println(strike + TEXT_RESULT_STRIKE + ", " + ball + TEXT_RESULT_BALL);
+        }
     }
 
 
