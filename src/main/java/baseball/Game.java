1d63c8103e85e0b122051e3aaf3699c71d159c55
Jongkook <barley.son@Jongkooks-MacBook-Pro.local>
2022-04-16 19:33:11 +0900

    feat: show hint and game over

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -9,8 +9,13 @@ import java.util.List;
 public class Game {
 
     private final int NUMBER_COUNT = 3;
-    private final String INPUT_NUMBER = "숫자를 입력해주세요 : ";
-    List<Integer> list = new ArrayList<>();
+    private final String TEXT_INPUT_NUMBER = "숫자를 입력해주세요 : ";
+    private final String TEXT_RESULT_STRIKE = "스트라이크";
+    private final String TEXT_RESULT_BALL = "볼";
+    private final String TEXT_RESULT_NOTHING = "낫싱";
+    private final String TEXT_RESULT_CORRECT = "3개의 숫자를 모두 맞히셨습니다! 게임종료";
+
+    List<Integer> answer = new ArrayList<>();
 
     Game() {
         // 중복 제거를 위한 HashSet
@@ -18,22 +23,100 @@ public class Game {
         while (set.size() < NUMBER_COUNT) {
             set.add(Randoms.pickNumberInRange(1, 9));
         }
-        list = new ArrayList<>(set);
+        answer = new ArrayList<>(set);
 
         // todo : 확인용. 개발 이후 제거 예정
-        for (int i = 0; i < list.size(); i++) {
-            System.out.println(list.get(i));
+        for (int i = 0; i < answer.size(); i++) {
+            System.out.println(answer.get(i));
         }
     }
 
     void play() {
-        System.out.print(INPUT_NUMBER);
-        String playerInput = Console.readLine();
-        showHint(playerInput);
+        boolean isPlaying = true;
+        while (isPlaying) {
+            System.out.print(TEXT_INPUT_NUMBER);
+            String playerInput = Console.readLine();
+            List<Integer> input = getInputFromString(playerInput);
+            int sCount = countStrike(input);
+            int bCount = countBall(input);
+            showStrikeAndBall(sCount, bCount);
+            isNothing(sCount, bCount);
+            isPlaying = !isGameOver(sCount);
+        }
     }
 
-    void showHint(String playerInput) {
-        System.out.println("show hint...");
+    List<Integer> getInputFromString(String playerInput) {
+        List<Integer> input = new ArrayList<>();
+        for (int i = 0; i < playerInput.length(); i++) {
+            input.add(playerInput.charAt(i) - '0');
+        }
+
+        // todo : 확인용. 개발 이후 제거 예정
+        for (int i = 0; i < input.size(); i++) {
+            System.out.println(input.get(i));
+        }
+
+        return input;
+    }
+
+    int countStrike(List<Integer> input) {
+        int count = 0;
+        for (int i = 0; i < NUMBER_COUNT; i++) {
+            count = count + checkStrike(answer.get(i), input.get(i));
+        }
+        return count;
+    }
+
+    int checkStrike(int answerNumber, int inputNumber) {
+        if (answerNumber == inputNumber) {
+            return 1;
+        }
+        return 0;
+    }
+
+    int countBall(List<Integer> input) {
+        int count = 0;
+        for (int i = 0; i < NUMBER_COUNT; i++) {
+            count = count + checkBall(answer.get(i), input.get(i));
+        }
+        return count;
+    }
+
+    int checkBall(int answerNumber, int inputNumber) {
+        if (answerNumber != inputNumber && answer.contains(inputNumber)) {
+            return 1;
+        }
+        return 0;
+    }
+
+    void showStrikeAndBall(int strike, int ball) {
+        if (strike + ball == 0) {
+            return;
+        }
+        if (strike == 0) {
+            System.out.println(ball + TEXT_RESULT_BALL);
+            return;
+        }
+        if (ball == 0) {
+            System.out.println(strike + TEXT_RESULT_STRIKE);
+            return;
+        }
+        System.out.println(strike + TEXT_RESULT_STRIKE + ", " + ball + TEXT_RESULT_BALL);
+    }
+
+
+    void isNothing(int strike, int ball) {
+        if (strike + ball == 0) {
+            System.out.println(TEXT_RESULT_NOTHING);
+        }
+    }
+
+    boolean isGameOver(int strike) {
+        if (strike == 3) {
+            System.out.println(TEXT_RESULT_CORRECT);
+            return true;
+        }
+        return false;
     }
 
 }
