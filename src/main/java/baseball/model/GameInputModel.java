a87aa7fce51572d518b16ab88ceeb0c041a25265
Jongkook <barley.son@Jongkooks-MacBook-Pro.local>
2022-04-17 21:47:34 +0900

    test: refactor and unit test on controller

--- src/main/java/baseball/model/GameInputModel.java
+++ src/main/java/baseball/model/GameInputModel.java
@@ -1,12 +1,6 @@
 package baseball.model;
 
-import java.util.List;
-
 public class GameInputModel extends GameModel {
-
-    public List<Integer> getInput() {
-        return list;
-    }
 
     public void setInput(Integer input) {
         this.list.add(input);
